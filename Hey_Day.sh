b1e31d25626a4809af9a699357e899957b6b16ae
mag0127 <146005253+mag0127@users.noreply.github.com>
2024-10-07 17:37:05 -0500

    Add files via upload

---
+++ Hey_Day.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This displays the question to the person running the script
+echo "Hi, what's you're name?"
+#This takes the user's input and stores it as a variable
+read name
+#this repeats the user's input in a statement
+echo "Hello $name, it's nice to meet you!"
+
+
+#This code was made with the help of Geeksforgeeks.com
+	# GeeksforGeeks. (10/7/2024). Read command in Linux with examples. 
+	# GeeksforGeeks. https://www.geeksforgeeks.org/read-command-in-linux-with-examples/
+
